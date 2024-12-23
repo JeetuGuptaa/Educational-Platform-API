486625c9c5ec1ad12107f5d79b783683d625dbe4
Jeetu Gupta <jeetu270602@gmail.com>
2024-12-23 15:02:25 +0530

    db connection

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const databaseConnection = require("./db/database");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes"));
+
+databaseConnection();
 
 app.listen(port, (err) => {
   if (err) console.log(err);
